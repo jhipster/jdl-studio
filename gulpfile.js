dc636b52fcc2328907dbc1aa0bfe0c0745b7d2fb
Deepu k Sasidharan <d4udts@gmail.com>
2016-08-06 13:20:00 +0530

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,66 @@ var gulp = require("gulp"),
     browserSync = require('browser-sync').create(),
     reload      = browserSync.reload;
 
+var bowerLibFiles = require('main-bower-files');
+
+var CodeMirrorFiles = [
+    'closebrackets.js',
+    'codemirror-compressed.js',
+    'codemirror.css',
+    'codemirror.jdl-mode.js',
+    'dialog.js',
+    'jdl-hint.js',
+    'searchcursor.js',
+    'show-hint.css',
+    'show-hint.js',
+    'solarized.jdl.css'
+];
+var nomnomlFiles = [
+    'skanaar.canvas.js',
+    'skanaar.util.js',
+    'skanaar.vector.js',
+    'nomnoml.parser.custom.js',
+    'nomnoml.layouter.custom.js',
+    'nomnoml.renderer.custom.js',
+    'nomnoml.custom.js'
+];
+
+var appFiles = [
+    'css/app.css',
+    'js/app.js'
+];
+
+var config = {
+    lib: 'lib'
+}
+
+gulp.task('copy:lib', function () {
+    gulp.src(bowerFiles(), {read: false})
+    .pipe(gulp.dest(config.lib))
+});
+
+gulp.task('build', function () {
+
+});
+
+gulp.task('inject', ['inject:dep', 'inject:app']);
+
+gulp.task('inject:app', function () {
+
+});
+
+
+gulp.task('inject:dep', function () {
+    /*var stream = gulp.src('index.html')
+        .pipe(inject(gulp.src(bowerFiles(), {read: false}), {
+            name: 'bower',
+            relative: true
+        }))
+        .pipe(gulp.dest(''));*/
+
+    //return stream;
+});
+
 gulp.task('serve', function () {
 
     // Serve files from the root of this project
