964dcf60f196387670a40d4ad8cc9187b338db03
deepu105 <d4udts@gmail.com>
2020-09-14 13:55:26 +0200

    add publish script

---
+++ publish.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo ">>> Working directory is clean"
+    TMP_LOC=/tmp/jdl-studio
+    DEPLOY_BRANCH=gh-pages
+    SRC_BRANCH=src
+
+    /bin/rm -rf build || exit
+    /bin/rm -rf $TMP_LOC || exit
+
+    echo ">> Building app"
+    npm run build || exit
+
+    echo ">> Move build assets to temp folder"
+    mkdir --parents $TMP_LOC || exit
+    mv build/* $TMP_LOC || exit
+
+    echo ">> Checkout and clean $DEPLOY_BRANCH branch"
+    git checkout $DEPLOY_BRANCH || exit
+    find -mindepth 1 -depth -print0 | grep -vEzZ '(.github(/|$)|node_modules(/|$)|.tmp(/|$)|.git(/|$)|/\.gitignore$|/\LICENCE.txt$|/\README.md$)' | xargs -0 rm -rvf || exit
+
+    echo ">> Move app form temp & publish to GitHub"
+    mv $TMP_LOC/* . || exit
+    now=$(date)
+    git add --all || exit
+    git commit -am "Updated app on $now" || exit
+    git push origin $DEPLOY_BRANCH --force || exit
+
+    echo ">> $now: Published changes to GitHub"
+
+    git checkout $SRC_BRANCH
+else
+    echo ">> Working directory is not clean. Commit changes and try again!"
+    exit
+fi
