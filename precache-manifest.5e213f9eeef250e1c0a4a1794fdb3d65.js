53c2384e6db245a88cf2e7e0438182b5dab08513
deepu105 <d4udts@gmail.com>
2020-09-14 17:41:10 +0200

    Updated app on Monday 14 September 2020 05:41:10 PM CEST

---
+++ precache-manifest.5e213f9eeef250e1c0a4a1794fdb3d65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc54769e8aecbaeb6938a6b9359ad01a",
+    "url": "/jdl-studio/index.html"
+  },
+  {
+    "revision": "b88fc006bf3a7623c6f7",
+    "url": "/jdl-studio/static/css/2.dd21d29c.chunk.css"
+  },
+  {
+    "revision": "05d7eb777d960eb02e37",
+    "url": "/jdl-studio/static/css/main.066454c1.chunk.css"
+  },
+  {
+    "revision": "b88fc006bf3a7623c6f7",
+    "url": "/jdl-studio/static/js/2.51d841b7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/jdl-studio/static/js/2.51d841b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d7eb777d960eb02e37",
+    "url": "/jdl-studio/static/js/main.1bd4b04a.chunk.js"
+  },
+  {
+    "revision": "e9765bdcd07a45ec0d83",
+    "url": "/jdl-studio/static/js/runtime-main.7ad85f61.js"
+  },
+  {
+    "revision": "335cc5d4733695820b4c5d8c9c4a7933",
+    "url": "/jdl-studio/static/media/logo-jhipster.335cc5d4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.193c051a55576462ff178b3518ff69f7.js"
+  "/jdl-studio/precache-manifest.5e213f9eeef250e1c0a4a1794fdb3d65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jdl-studio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
