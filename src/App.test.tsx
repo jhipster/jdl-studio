87d2b9a3669d6d1b64b415bbe84b4b1174c57cb4
Deepu <d4udts@gmail.com>
2023-08-23 15:26:41 +0200

    migrate to happy-dom for tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,47 +1,35 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
-// import "@testing-library/jest-dom";
 import App from "./App";
 import { store } from "./Store";
 
-import * as jsdom from "jsdom";
-const doc = new jsdom.JSDOM("<!doctype html><html><body></body></html>");
-global.document = doc;
-global.window = doc.defaultView;
-//@ts-ignore
-global.document.body.createTextRange = function () {
-  return {
-    setEnd: function () {},
-    setStart: function () {},
-    getBoundingClientRect: function () {
-      return { right: 0 };
-    },
-    getClientRects: function () {
-      return {
-        length: 0,
-        left: 0,
-        right: 0,
-      };
-    },
-  };
-};
+import { Window } from "happy-dom";
+
+const window = new Window({
+  url: "https://localhost:8080",
+  width: 1024,
+  height: 768,
+});
+global.window = window as any;
+global.document = window.document as any;
+
 
 test("renders about page text", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>,
   );
   const linkElement = getByText(/Hello, this is JDL-Studio/i);
-  expect(linkElement).toBeDefined()
+  expect(linkElement).toBeDefined();
 });
 
 test("renders default snapshot", () => {
   const comp = render(
     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>,
   );
   expect(comp).toMatchSnapshot();
 });
--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   ],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: "./src/setupTests.ts",
     coverage: {
       reporter: ["text", "html"],
