f0b90c53306f148d0e722535c848b613b7624845
Deepu <d4udts@gmail.com>
2023-08-23 13:14:52 +0200

    move to vite

--- src/components/studio/JDLToNoml.ts
+++ src/components/studio/JDLToNoml.ts
@@ -39,7 +39,7 @@ export function jdlToNoml(jdlString: string): string {
     .join("\n")
     .trim();
 
-  const JDL = parse(pureDiagramCode);
+  const JDL = JdlLib.parse(pureDiagramCode);
   let nomlEntities: NomlEntity[] = [];
   const allEntityNames = JDL.entities.map((it) => it.name);
   let unProcessedEntities: string[] = [...allEntityNames];
